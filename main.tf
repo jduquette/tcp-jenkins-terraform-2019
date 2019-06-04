5c5443a6c8daeb69f34047045fe467797440d53a
johnduquette <johnduquette@10.23.6.54>
2019-06-04 15:14:34 -0400

    first crack at ASG

--- main.tf
+++ main.tf
@@ -5,38 +5,38 @@ provider "aws" {
 ######################################
 # Create the necessary IAM ROLE and POLICIES
 ######################################
-#module "iam_role" {
-#	prefix = "${var.prefix}"
-#	ProjectName = "${var.ProjectName}"
-#	aws_email = "${var.aws_email}"
-#	source = "./modules/iam/role"		
-#}
+module "iam_role" {
+  prefix      = "${var.prefix}"
+  ProjectName = "${var.ProjectName}"
+  aws_email   = "${var.aws_email}"
+  source      = "./modules/iam/role"
+}
 
 ######################################
 # Create custom policy
 ######################################
-#module "iam_policy" {
-#	prefix = "${var.prefix}"
-#	ProjectName = "${var.ProjectName}"
-#	aws_email = "${var.aws_email}"
-#	source = "./modules/iam/policy"
-#}
+module "iam_policy" {
+  prefix      = "${var.prefix}"
+  ProjectName = "${var.ProjectName}"
+  aws_email   = "${var.aws_email}"
+  source      = "./modules/iam/policy"
+}
 
 ######################################
 # Attach custom policy
 ######################################
-#resource "aws_iam_role_policy_attachment" "attach-JenkinsPolicy" {
-#  role       = "${module.iam_role.iam_role_name}"
-#  policy_arn = "arn:aws:iam::090999229429:policy/${module.iam_policy.iam_policy_name}"
-#}
+resource "aws_iam_role_policy_attachment" "attach-JenkinsPolicy" {
+  role       = "${module.iam_role.iam_role_name}"
+  policy_arn = "arn:aws:iam::090999229429:policy/${module.iam_policy.iam_policy_name}"
+}
 
 ######################################
 # Attach ReadOnly policy (?)
 ######################################
-#resource "aws_iam_role_policy_attachment" "attach-ReadOnly" {
-#  role       = "${module.iam_role.iam_role_name}"
-#  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
-#}
+resource "aws_iam_role_policy_attachment" "attach-ReadOnly" {
+  role       = "${module.iam_role.iam_role_name}"
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
 
 ######################################
 # Create Security Group for LoadBalancer
@@ -56,12 +56,10 @@ module "lb_sg" {
     Team            = var.Team
   }
 }
-
 ######################################
 # Create Security Group for EC2 Instance
 ######################################
 module "app_sg" {
-  #source      = "terraform-aws-modules/security-group/aws//modules/http-8080"
   source       = "terraform-aws-modules/security-group/aws"
   name         = "${var.prefix}-tcp-app"
   description  = "Security Group for Jenkins Application Server"
@@ -74,7 +72,6 @@ module "app_sg" {
     }
   ]
   number_of_computed_ingress_with_source_security_group_id = 1
-
   tags = {
     Server_Function = "DEV_APP",
     System          = "ATA",
@@ -83,4 +80,90 @@ module "app_sg" {
     Owner           = var.aws_email,
     Team            = var.Team
   }
+}
+
+######################################
+# Create Instance Profile
+######################################
+resource "iam_instance_profile" "profile" {
+  name = "${var.prefix}-instance-profile"
+  role = "${module.iam_role.iam_role_name}"
+}
+
+######################################
+# Create AutoScaling Group
+######################################
+module "app_asg" {
+  source               = "terraform-aws-modules/autoscaling/aws"
+  name                 = "Duquette-TCP-JENKINS-ASG"
+  lc_name              = "Duquette_launch_config"
+  image_id             = var.AmiID
+  instance_type        = "t2.xlarge"
+  security_groups      = ["${module.app_sg}"]
+  iam_instance_profile = "${iam_instance_profile.profile.name}"
+  key_name             = "josh.phillips"
+  ebs_block_device = [
+    {
+      device_name           = "/dev/sde"
+      volume_type           = "gp2"
+      volume_size           = "400"
+      delete_on_termination = true
+    }
+  ]
+  root_block_device = [
+    {
+      volume_size           = "50"
+      volume_type           = "gp2"
+      delete_on_termination = true
+    }
+  ]
+  user_data = <<-EOF
+  			#!/bin/bash --login
+
+
+            export HOME=/var/chef/solo;
+            export AWS_REGION=${region};
+            export kmskeyid=${kms_key_id};
+            
+            
+            # get attributes json
+            
+            
+            crossing get s3://${bucket_name}/jenkins/deployment_id/attributes.json /var/chef/solo/;
+            echo 'attributes json is downloaded';
+            
+            
+            # get data_bags objects
+            
+            
+            mkdir -p /var/chef/solo/data_bags/jenkins;
+            crossing get s3://${bucket_name}/jenkins/deployment_id/data_bags/credentials.json /var/chef/solo/data_bags/jenkins;
+            crossing get s3://${bucket_name}/jenkins/deployment_id/data_bags/local_users.json /var/chef/solo/data_bags/jenkins;
+            echo 'data_bags are downloaded';
+            
+            
+            # running chef for default_firstboot recipe
+
+
+            chef-solo \
+              -j /var/chef/solo/attributes.json \
+              -c /var/chef/solo/solo.rb \
+              -l info -L /var/log/chef.log;
+
+
+            # delete credentials
+
+
+            rm -rf data_bags;
+            service jenkins restart;
+            cfn-signal --exit-code $? --stack ${stack_name} --resource ${asg_name} --region ${region};
+            EOF
+  asg_name = "var.prefix--TCP-JENKINS"
+  vpc_zone_identifier = ["subnet-018118375aa52d481"]
+  health_check_type = "ELB"
+  health_check_grace_period = "300"
+  min_size = 1
+  max_size = 1
+  desired_capacity = 1
+  wait_for_capacity_timeout = 0
 }
--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
-#output "IAM_ROLE_ARN" {
-#  value = "${aws_iam_role.role.arn}"
-#}
+output "IAM_ROLE_ARN" {
+  value = "${aws_iam_role.role.arn}"
+}
 
-#output "iam_role_name" {
-#	value = "${module.iam_role.iam_role_name}"
-#}
+output "iam_role_name" {
+  value = "${module.iam_role.iam_role_name}"
+}
 
 output "lb_sg_name" {
   value = "${module.lb_sg.this_security_group_name}"
@@ -12,4 +12,12 @@ output "lb_sg_name" {
 
 output "lb_sg_id" {
   value = "${module.lb_sg.this_security_group_id}"
+}
+
+output "app_sg_id" {
+  value = "${module.app_sg.this_security_group_id}"
+}
+
+output "profile_name" {
+  value = "${aws_iam_instance_profile.profile.name}"
 }
